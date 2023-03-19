4e9a670e7fb4ab20b817496509f653bb5e1c1744
muntiCloud <muntazacloud@gmail.com>
2023-03-19 16:26:23 -0700

    add

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import '../src/assets/App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+     <h1>Hello World </h1>
+    </div>
+  );
+}
